5609668b8c0635bed1cb960dad0436752c3b610e
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-05-29 13:22:41 +1000

    Add license

--- src/score.ts
+++ src/score.ts
@@ -1,3 +1,21 @@
+/*
+* FuzzJudge - Randomised input judging server, designed for ProgComp.
+* Copyright (C) 2024 UTS Programmers' Society (ProgSoc)
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { FuzzJudgeProblem } from "./comp.ts";
 import { DB } from "./deps.ts"
 
@@ -44,7 +62,7 @@ export function getScoreboard(problems: Record<string, FuzzJudgeProblem>): UserS
 export function appendAnswer(username: string, slug: string) {
   let answered = getAnswered(username);
 
-  if (answered.find((a) => a.slug === slug) !== undefined) return;
+  if (answered.some((a) => a.slug === slug)) return;
 
   answered.push({ slug, time: new Date(Date.now()) });
 
