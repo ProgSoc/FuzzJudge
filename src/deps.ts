e089299eb3ba307e58e122b74e584900acc0d202
Oli Lenehan <oli@sunsetkookaburra.net>
2024-06-20 10:47:50 +1000

    feat: json router

--- src/deps.ts
+++ src/deps.ts
@@ -4,3 +4,5 @@ export * as YAML from "https://deno.land/std@0.223.0/yaml/mod.ts";
 export { join as pathJoin, dirname, basename } from "https://deno.land/std@0.223.0/path/mod.ts";
 export { render as renderMarkdown } from "https://deno.land/x/gfm@0.6.0/mod.ts";
 export { DB } from "https://deno.land/x/sqlite/mod.ts";
+export { serveFile } from "https://jsr.io/@std/http/0.224.5/mod.ts"
+export { normalize } from "https://jsr.io/@std/path/0.225.2/mod.ts"
