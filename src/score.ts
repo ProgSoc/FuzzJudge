ecdf30751cdfc2988d091fb8a4725d65a28ba1d9
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-06-21 20:16:19 +0900

    fix: incorrect score sums

--- src/score.ts
+++ src/score.ts
@@ -57,7 +57,7 @@ export function notifySubscribers() {
 }
 
 export function calculatePoints(problems: Record<string, FuzzJudgeProblem>, answers: Answer[]) {
-  return answers.reduce((acc, answer) => problems[answer.slug]?.doc().config?.problem?.points ?? 0 + acc, 0);
+  return answers.reduce((acc, answer) => (problems[answer.slug]?.doc().config?.problem?.points ?? 0) + acc, 0);
 }
 
 export function getScoreboard(problems: Record<string, FuzzJudgeProblem>): UserScore[] {
