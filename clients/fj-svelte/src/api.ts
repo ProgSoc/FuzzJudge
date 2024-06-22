7476f01d8da8f9ce76edc1db99fb28759739ddaa
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-06-22 14:55:04 +0900

    feat(fj-svelte): add license

--- clients/fj-svelte/src/api.ts
+++ clients/fj-svelte/src/api.ts
@@ -1,3 +1,20 @@
+/*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+* for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License along
+* with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
 import { BACKEND_SERVER, parse_scoreboard, parse_times, question_order, type CompTimes, type QuestionMeta, type ScoreboardUser} from "./utils";
 
 export let get_questions = async (): Promise<Record<string, QuestionMeta>> => {
