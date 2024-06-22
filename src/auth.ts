8e12e608238903a32c4cdcd5df520d30f75cf885
Arduano <leonid.shchurov@gmail.com>
2024-06-23 09:45:14 +1000

    style: Repo-wide prettier
    Uses `format.sh` for now, we can adjust that later. But it's good to get consistent formatting out of the way now first.

--- src/auth.ts
+++ src/auth.ts
@@ -1,26 +1,23 @@
 /*
-* FuzzJudge - Randomised input judging server, designed for ProgComp.
-* Copyright (C) 2024 UTS Programmers' Society (ProgSoc)
-*
-* This program is free software: you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published
-* by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * FuzzJudge - Randomised input judging server, designed for ProgComp.
+ * Copyright (C) 2024 UTS Programmers' Society (ProgSoc)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 export interface AuthSchemes<UserDetails> {
-  basic: (options: {
-    username: string,
-    password: string,
-  }) => UserDetails | null | Promise<UserDetails | null>,
+  basic: (options: { username: string; password: string }) => UserDetails | null | Promise<UserDetails | null>;
 }
 
 export class Auth<UserDetails> {
@@ -35,7 +32,7 @@ export class Auth<UserDetails> {
     if (header) {
       if (header.startsWith("Basic ")) {
         try {
-          const [ username, password ] = atob(header.slice(6)).split(/:(.*)/);
+          const [username, password] = atob(header.slice(6)).split(/:(.*)/);
           if (username == "") this.reject();
           const details = await this.#schemes.basic({ username, password });
           if (details !== null) return details;
