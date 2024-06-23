a7d8386cc073c0634691b9587d9089f46f2ed92f
arduano <leonid.shchurov@gmail.com>
2024-06-23 15:46:05 +1000

    fix: Improved countdowns and time state (#61)

---
+++ clients/fj-svelte/src/lib/counters/InlineCountdown.svelte
@@ -0,0 +1,42 @@
+<!--
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<script lang="ts">
+  import { CompState, unreachable, type TimeStateData, type CompTimes } from "../../utils";
+  import { dateToTimeString, secondsToString } from "./shared";
+
+  export let timeStateData: TimeStateData;
+</script>
+
+{#if timeStateData.phase === CompState.BEFORE}
+  <span>Competition starts in {secondsToString(timeStateData.secondsUntilCompetitionStart)}</span>
+{:else if timeStateData.phase === CompState.LIVE_WITH_SCORES}
+  <span>
+    Competition ends in {secondsToString(timeStateData.secondsUntilCompetitionEnd)}. The scoreboard will freeze at {dateToTimeString(
+      timeStateData.times.freeze,
+    )}
+  </span>
+{:else if timeStateData.phase === CompState.LIVE_WITHOUT_SCORES}
+  <span>
+    Competition ends in {secondsToString(timeStateData.secondsUntilCompetitionEnd)}.
+  </span>
+{:else if timeStateData.phase === CompState.FINISHED}
+  <span>Competition finished! You can no longer submit solutions.</span>
+{:else}
+  {unreachable(timeStateData.phase)}
+{/if}
+
+<style>
+</style>
