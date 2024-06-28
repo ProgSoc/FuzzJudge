3314b394645c807def3eef65cddd7ed082c5470c
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-06-28 16:13:56 +1000

    feat(clients/fj-tui): line wrap util

--- clients/fj-tui/src/utils.rs
+++ clients/fj-tui/src/utils.rs
@@ -13,3 +13,34 @@ pub fn dump_to_file(conents: &str, path: &str) -> std::io::Result<()> {
     file.write_all(conents.as_bytes())?;
     Ok(())
 }
+
+pub fn number_of_lines_when_broken(s: &str, width: usize) -> usize {
+    let mut lines = 1;
+    let mut column = 0;
+    let mut last_word_start = 0;
+    let mut in_word = false;
+
+    let mut i = 0;
+
+    while i < s.chars().count() {
+        let c = s.chars().nth(i).unwrap();
+
+        if !in_word && !c.is_whitespace() {
+            in_word = true;
+            last_word_start = i;
+        } else if in_word && c.is_whitespace() {
+            in_word = false;
+        }
+
+        if column == width {
+            lines += 1;
+            column = 0;
+            i = last_word_start;
+        }
+
+        column += 1;
+        i += 1;
+    }
+
+    lines
+}
