439e32e2a4b1e7ac80f93e04a034f657ecc61b92
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-06-25 11:38:22 +1000

    Fix passwd hashing

--- server/src/schema/users/resolvers/Mutation/createUser.ts
+++ server/src/schema/users/resolvers/Mutation/createUser.ts
@@ -2,16 +2,19 @@ import { db } from "@/db";
 import { userTable } from "@/db/schema";
 import { GraphQLError } from "graphql";
 import type { MutationResolvers } from "./../../../types.generated";
+import { hashPassword } from "@/auth/password";
 export const createUser: NonNullable<MutationResolvers["createUser"]> = async (
 	_parent,
 	{ role, teamId, username, password, name },
 ) => {
+	const hashed = await hashPassword(password);
+
 	const [newUser] = await db
 		.insert(userTable)
 		.values({
 			name,
 			username,
-			password,
+			password: hashed,
 			role,
 			teamId,
 		})
